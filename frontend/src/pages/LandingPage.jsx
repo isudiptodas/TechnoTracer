daa455ccd2477b791cf0b2899a59c69e6fee0bdb
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-04-30 01:56:11 +0530

    replaced footer code with component at landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -5,6 +5,7 @@ import CountUp from 'react-countup';
 import Marquee from "react-fast-marquee";
 import Accordian from "../components/Accordian";
 import { faq } from '../data/accordianFAQ.js';
+import Footer from "../components/Footer.jsx";
 
 function LandingPage() {
 
@@ -77,15 +78,7 @@ function LandingPage() {
         </div>
 
         {/* footer */}
-        <div className="w-full rounded-t-4xl h-64 flex flex-col justify-start items-center py-5 px-4 bg-gradient-to-br from-orange-400 via-yellow-400 to-emerald-500">
-          <h1 className="w-full flex justify-center items-center md:py-5 z-30 text-black font-bold font-DuneRise text-[10px] md:text-sm xl:text-lg">TECHNOTRACER</h1>
-
-          <div className="w-full mt-10 flex flex-col justify-center items-center gap-2 lg:gap-10 md:flex-row">
-            <Link to='/about' className="font-Montserrat text-sm xl:text-lg hover:font-semibold cursor-pointer hover:scale-110 duration-200 ease-in-out">About</Link>
-            <Link to='/privacy' className="font-Montserrat text-sm xl:text-lg hover:font-semibold cursor-pointer hover:scale-110 duration-200 ease-in-out">Privacy Policy</Link>
-            <Link to='terms' className="font-Montserrat text-sm xl:text-lg hover:font-semibold cursor-pointer hover:scale-110 duration-200 ease-in-out">Terms of Use</Link>
-          </div>
-        </div>
+        <Footer/>
 
       </div>
     </>
