4294be1b60c3a45c1d7051275e1c53205f65ece8
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-05-01 01:54:17 +0530

    college and states array
    added college and states array for user profile updation

---
+++ frontend/src/data/states.js
@@ -0,0 +1,36 @@
+export const states = [
+    'andhra pradesh',
+    'arunachal pradesh', 
+    'assam',
+    'bihar',
+    'chhattisgarh',
+    'goa',
+    'gujarat',
+    'haryana',
+    'himachal pradesh',
+    'jharkhand',
+    'karnataka',
+    'kerala',
+    'madhya pradesh',
+    'maharashtra',
+    'manipur',
+    'meghalaya',
+    'mizoram',
+    'nagaland',
+    'odisha',
+    'punjab',
+    'rajasthan',
+    'sikkim',
+    'tamil nadu',
+    'telangana',
+    'tripura',
+    'uttar pradesh',
+    'uttarakhand',
+    'west bengal'
+];
+
+// arunachal pradesh
+// west bengal
+// delhi
+// maharashtra
+// telengana
