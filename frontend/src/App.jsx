f60a23ec9bd9b80387eedf208e97a9b9c10c22b0
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-05-01 21:44:14 +0530

    added user claim and past listing routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,9 @@ import About from "./pages/About.jsx";
 import Privacy from "./pages/Privacy.jsx";
 import Terms from "./pages/Terms.jsx";
 import Settings from "./pages/user/Settings.jsx";
+import Report from "./pages/user/Report.jsx";
+import PastListing from "./pages/user/PastListing.jsx";
+import ClaimPage from "./pages/user/ClaimPage.jsx";
 
 import { Routes, Route } from 'react-router-dom';
 
@@ -15,8 +18,13 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/auth" element={<Auth/>}/>
+
         <Route path="/user/dashboard" element={<Dashboard/>}/>
+        <Route path="/user/report" element={<Report/>}/>
+        <Route path="/user/past-listing" element={<PastListing/>}/>
         <Route path="/user/settings" element={<Settings/>}/>
+        <Route path="/user/item/details" element={<ClaimPage/>}/>
+
         <Route path="/admin/auth" element={<AdminAuth/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/terms" element={<Terms/>}/>
