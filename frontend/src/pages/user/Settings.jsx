3dcc66dc60620faf254316b271327096dbcbdab2
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-06-12 00:25:26 +0530

    added profile updation for user

--- frontend/src/pages/user/Settings.jsx
+++ frontend/src/pages/user/Settings.jsx
@@ -1,7 +1,7 @@
 import MenuBar from '../../components/MenuBar.jsx'
 import Footer from '../../components/Footer.jsx'
 import profile from '../../assets/profile.jpg';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, Suspense } from 'react';
 import { FaTrashAlt } from "react-icons/fa";
 import { IoIosCloudUpload } from "react-icons/io";
 import Vapi from "@vapi-ai/web";
@@ -9,16 +9,76 @@ import { FaRegCircleStop } from "react-icons/fa6";
 import { LuSparkle } from "react-icons/lu";
 import { GoOrganization } from "react-icons/go";
 import { FaGlobeAmericas } from "react-icons/fa";
+import axios from 'axios';
+import loading from '../../assets/loading.gif';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
+import { FaLock } from "react-icons/fa";
+import { FaUserCheck } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
 
 function Settings() {
 
     const [profileVisible, setProfileVisible] = useState(false);
     const [uploadVisible, setUploadVisible] = useState(false);
+    const [verified, setVerified] = useState(false);
+    const[passwordVisible, setPasswordVisible] = useState(false);
     const [image, setImage] = useState(null);
+    const [userDetails, setUserDetails] = useState(null);
+    const [contact, setContact] = useState('');
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [current, setCurrent] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const navigate = useNavigate();
 
     const [started, setStarted] = useState(false);
 
     const vapi = useRef(null);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+
+                const res = await axios.get('http://localhost:8080/user/details', {
+                    withCredentials: true
+                });
+
+                //console.log(res.data);
+                setUserDetails(res.data);
+                setName(res.data.username);
+                setEmail(res.data.email);
+                setContact(res.data.contact);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        const verifyUser = async () => {
+            try {
+                const res = await axios.get('http://localhost:8080/user/verify', {
+                    withCredentials: true
+                });
+
+                //console.log(res);
+
+                if (res.data === true) {
+                    setVerified(true);
+                }
+                else {
+                    navigate('/');
+                }
+            } catch (err) {
+                //console.log(err);
+                navigate('/');
+            }
+        }
+
+        verifyUser();
+
+        fetchUser();
+    }, []);
 
     useEffect(() => {
         vapi.current = new Vapi(import.meta.env.VITE_VAPI_PUBLIC);
@@ -69,9 +129,96 @@ function Settings() {
         setImage(e);
     }
 
+    const updateProfile = async () => {
+        if (name.trim() === '' || email.trim() === '' || contact.trim() === '') {
+            toast.error("Empty fields are not allowed");
+            return;
+        }
+
+        let id;
+
+        try {
+            id = toast.loading("Updating . . .");
+            const res = await axios.put(`http://localhost:8080/user/update-profile?previous=${encodeURIComponent(userDetails?.email)}`, {
+                name: name,
+                email: email || userDetails.email,
+                contact: contact,
+                image: image,
+            }, {
+                withCredentials: true
+            });
+
+            if (res.status === 200) {
+                toast.dismiss(id);
+                toast.success("Profile Updated");
+                setTimeout(() => {
+                    navigate('/user/dashboard');
+                }, 2000);
+            }
+        } catch (err) {
+            if (err?.response?.data) {
+                toast.error(err.response.data)
+            }
+            else {
+                toast.error("Something went wrong");
+            }
+            console.log(err);
+        }
+        finally {
+            toast.dismiss(id);
+        }
+    }
+
+    const changePassword = async () => {
+        if (!newPassword || !current) {
+            toast.error("Please add current and new password");
+            return;
+        }
+
+        if(newPassword.trim().length <= 7){
+            toast.error("New password length must be 8 or more");
+            return;
+        }
+
+        let id;
+
+        try {
+            id = toast.loading("Updating . . .");
+            const res = await axios.put(`http://localhost:8080/user/update-password?email=${encodeURIComponent(userDetails?.email)}`, {
+                currentPassword: current,
+                newPassword: newPassword
+            }, {
+                withCredentials: true
+            });
+
+            if (res.status === 200) {
+                toast.dismiss(id);
+                toast.success("Password Changed");
+                setTimeout(() => {
+                    navigate('/user/dashboard');
+                }, 2000);
+            }
+        } catch (err) {
+            if (err?.response?.data) {
+                toast.error(err.response.data)
+            }
+            else {
+                toast.error("Something went wrong");
+            }
+            console.log(err);
+        }
+        finally {
+            toast.dismiss(id);
+        }
+    }
+
     return (
         <>
-            <div className='w-full z-10 min-h-screen bg-black flex flex-col justify-start items-center overflow-hidden relative' >
+            <div className={`${verified ? "hidden" : "block"} h-screen w-full bg-black flex justify-center items-center`}>
+                <img src={loading} />
+            </div>
+
+            <div className={`${verified ? "block" : "hidden"} w-full z-10 min-h-screen bg-black flex flex-col justify-start items-center overflow-hidden relative`} >
                 <MenuBar />
 
                 <p onClick={askAI} className={`w-auto ${started ? "hidden" : "block"} z-30 px-5 text-[12px] hover:opacity-75 shadow-md lg:text-lg lg:right-10 lg:bottom-12 py-2 rounded-full cursor-pointer active:scale-95 duration-200 ease-in-out font-Montserrat bg-white text-black fixed bottom-9 right-5 flex justify-center items-center gap-2`}>Ask AI <LuSparkle /></p>
@@ -96,7 +243,7 @@ function Settings() {
 
                 {/* picture info div */}
                 <div className='h-auto py-3 w-[60%] flex flex-col justify-start px-2 items-center'>
-                    <p className='w-full text-center text-white text-2xl md:text-4xl font-Montserrat mb-2'>Sudipto Das</p>
+                    <p className='w-full text-center text-white text-2xl md:text-4xl font-Montserrat mb-2'>{userDetails?.username}</p>
                     <div className='w-full lg:w-[50%] xl:w-[40%] flex flex-col justify-center items-center lg:flex-row'>
                         <p className='w-full text-center text-red-500 text-[12px] md:text-sm cursor-pointer flex justify-center items-center gap-2 font-Montserrat mb-1'>Remove Picture <FaTrashAlt /></p>
                         <p onClick={() => setUploadVisible(!uploadVisible)} className='w-full text-center text-blue-400 text-[12px] md:text-sm cursor-pointer flex justify-center items-center gap-2 font-Montserrat'>Change Picture <IoIosCloudUpload /></p>
@@ -109,19 +256,29 @@ function Settings() {
                     </div>
                 </div>
 
-               <div className='w-full md:w-[70%] lg:w-[40%] flex flex-col justify-start items-start gap-2 mt-5 px-5'>
-               <p className='w-full text-white text-center font-Montserrat flex justify-center items-center gap-2'><span><GoOrganization /></span>Techno Main Salt Lake</p>
-               <p className='w-full text-white text-center font-Montserrat flex justify-center items-center gap-2'><span><FaGlobeAmericas /></span>West Bengal</p>
-               </div>
+                <div className='w-full md:w-[70%] lg:w-[40%] flex flex-col justify-start items-start gap-2 mt-5 px-5'>
+                    <p className='w-full text-white text-center font-Montserrat flex justify-center items-center gap-2'><span><GoOrganization /></span>{userDetails?.college}</p>
+                    <p className='w-full text-white text-center font-Montserrat flex justify-center items-center gap-2'><span><FaGlobeAmericas /></span>{userDetails?.state}</p>
+                </div>
 
                 {/* basic info */}
                 <div className='w-full my-5 md:w-[70%] lg:w-[40%] py-4 px-4 flex flex-col justify-start items-center gap-3 relative'>
 
-                    <input type="text" className='w-full rounded-md py-2 px-3 outline-none bg-zinc-800 text-white placeholder-gray-300' placeholder='Update name' />
-                    <input type="email" className='w-full rounded-md py-2 px-3 outline-none bg-zinc-800 text-white placeholder-gray-300' placeholder='Update email' />
-                    <input type="text" className='w-full rounded-md py-2 px-3 outline-none bg-zinc-800 text-white placeholder-gray-300' placeholder='Update contact' />
+                    <input onChange={(e) => setName(e.target.value)} value={name} type="text" className='w-full rounded-md py-2 px-3 outline-none bg-zinc-800 text-white placeholder-gray-300' placeholder='Update name' />
+                    <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" className='w-full rounded-md py-2 px-3 outline-none bg-zinc-800 text-white placeholder-gray-300' placeholder='Update email' />
+                    <input onChange={(e) => setContact(e.target.value)} value={contact} type="text" className='w-full rounded-md py-2 px-3 outline-none bg-zinc-800 text-white placeholder-gray-300' placeholder='Update contact' />
 
-                    <p className='w-full py-2 lg:py-3 text-center bg-white text-black capitalize rounded-md cursor-pointer hover:opacity-70 duration-200 ease-in-out font-Montserrat my-2 font-semibold active:scale-95'>Update profile</p>
+                    <p className='w-full py-2 lg:py-3 text-center bg-white text-black capitalize rounded-md cursor-pointer hover:opacity-70 duration-200 ease-in-out font-Montserrat my-2 font-semibold active:scale-95 flex justify-center items-center gap-2' onClick={updateProfile}>Update profile <FaUserCheck /></p>
+
+                    <div className='w-full h-auto py-2 flex flex-col justify-center items-center gap-2 relative'>
+                        <input onChange={(e) => setCurrent(e.target.value)} value={current} type={passwordVisible ? "text" : "password"} className='w-full mt-5 rounded-md py-2 px-3 pr-10 outline-none bg-zinc-800 text-white placeholder-gray-300' placeholder='Enter current password' />
+                        <span onClick={() => setPasswordVisible(!passwordVisible)} className='absolute text-white cursor-pointer opacity-55 top-10 right-5'>{passwordVisible ? <FaEye/> : <FaEyeSlash/>}</span>
+                        
+                        <input onChange={(e) => setNewPassword(e.target.value)} value={newPassword} type={passwordVisible ? "text" : "password"} className='w-full rounded-md py-2 px-3 pr-10 outline-none bg-zinc-800 text-white placeholder-gray-300' placeholder='Enter new password' />
+                        <span onClick={() => setPasswordVisible(!passwordVisible)} className='absolute text-white cursor-pointer opacity-55 top-[90px] right-5'>{passwordVisible ? <FaEye/> : <FaEyeSlash/>}</span>
+
+                        <p className='w-full py-2 lg:py-3 text-center bg-white text-black capitalize rounded-md cursor-pointer hover:opacity-70 duration-200 ease-in-out font-Montserrat my-2 font-semibold active:scale-95 flex justify-center items-center gap-2' onClick={changePassword}>Update Password <FaLock /></p>
+                    </div>
                 </div>
 
                 <div className="w-full h-44 lg:h-32"></div>
